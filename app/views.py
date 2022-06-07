d6f439e25e8b3cf99ca8c79b7b401df85844e194
Felipe-Droguett <felipedroguettf@gmail.com>
2022-06-07 05:56:43 -0400

    primer commit

---
+++ app/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def aguamineral(request):
+    return render(request,'app/aguamineral.html')
+
+def arroz(request):
+    return render(request,'app/arroz.html')
+
+def bebidas(request):
+    return render(request,'app/bebidas.html')
+
+def busqueda(request):
+    return render(request,'app/busqueda.html')
+
+def cafe(request):
+    return render(request,'app/cafe.html')
+
+def carritodecompras(request):
+    return render(request,'app/carritodecompras.html')
+
+def chocolate(request):
+    return render(request,'app/chocolate.html')
+
+def confort(request):
+    return render(request,'app/confort.html')
+
+def desodoranteamb(request):
+    return render(request,'app/desodoranteamb.html')
+
+def detergente(request):
+    return render(request,'app/detergente.html')
+
+def dulceambroso(request):
+    return render(request,'app/dulceambroso.html')
+
+def fiado(request):
+    return render(request,'app/fiado.html')
+
+def fideos(request):
+    return render(request,'app/fideos.html')
+
+def galleta(request):
+    return render(request,'app/galleta.html')
+
+def home(request):
+    return render(request,'app/home.html')
+
+def huevosdepascua(request):
+    return render(request,'app/huevosdepascua.html')
+
+def InicioSesion(request):
+    return render(request,'app/InicioSesion.html')
+
+def jugos(request):
+    return render(request,'app/jugos.html')
+
+def lavaloza(request):
+    return render(request,'app/lavaloza.html')
+
+def leche(request):
+    return render(request,'app/leche.html')
+
+def maruchan(request):
+    return render(request,'app/maruchan.html')
+
+def mediosdepago(request):
+    return render(request,'app/mediosdepago.html')
+
+def pastadedientes(request):
+    return render(request,'app/pastadedientes.html')
+
+def productos(request):
+    return render(request,'app/productos.html')
+
+def quienessomos(request):
+    return render(request,'app/quienessomos.html')
+
+def registrarse(request):
+    return render(request,'app/registrarse.html')
+
+def salsatomate(request):
+    return render(request,'app/salsatomate.html')
+
+def te(request):
+    return render(request,'app/te.html')
+
+def yoghurt(request):
+    return render(request,'app/yoghurt.html')
+
+def delivery(request):
+    return render(request,'app/delivery.html')
